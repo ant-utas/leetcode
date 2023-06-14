027f691eb45bcb322ff5e5e1d37d859b95c2f252
ant-utas <126833729+ant-utas@users.noreply.github.com>
2023-06-14 22:38:37 +1000

    Update add_two_numbers.c

--- add_two_numbers.c
+++ add_two_numbers.c
@@ -11,7 +11,7 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
     struct ListNode* result_holder = result;
     int over = 0;//when sum is greater than 10, carry the 1 to next node
     int sum;
-    while(l1!=NULL || l2!=NULL || over){//continue until both l1 and l2 are NULL
+    while((l1!=NULL) || (l2!=NULL) || over){//continue until both l1 and l2 are NULL
         if(l1 != NULL && l2 != NULL){
             sum = l1->val+l2->val;
             l1 = l1->next;
@@ -26,7 +26,6 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
             l1 = l1->next;
         }
         result->val = over + sum%10;
-        printf("%d\n",sum);
         over = 0;
         if(sum >= 10){
             over = 1;
@@ -36,9 +35,8 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
         new_node->next = NULL;
         result->next = new_node;
         result = result->next;
-        
-        printf("waaaah\n");
     }
     printf("- - - - - - - - - - - - - - - - - -\n");
+    result = NULL;
     return result_holder;
 }
