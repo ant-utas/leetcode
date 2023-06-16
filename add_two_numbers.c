1a62bbba10abfc4b1f624cbe2ff0f77137d6a704
ant-utas <126833729+ant-utas@users.noreply.github.com>
2023-06-16 17:21:18 +1000

    Update add_two_numbers.c

--- add_two_numbers.c
+++ add_two_numbers.c
@@ -6,37 +6,47 @@
  * };
  */
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
-    struct ListNode* result = malloc(sizeof(struct ListNode));
-    result->next = NULL;
-    struct ListNode* result_holder = result;
-    int over = 0;//when sum is greater than 10, carry the 1 to next node
+    int carry_over = 0;
     int sum;
-    while((l1!=NULL) || (l2!=NULL) || over){//continue until both l1 and l2 are NULL
-        if(l1 != NULL && l2 != NULL){
-            sum = l1->val+l2->val;
+    struct ListNode* result = malloc(sizeof(struct ListNode));
+    struct ListNode* result_holder = result;
+    struct ListNode* result_ender = result;
+    int quit = 0;
+    while(!quit){
+        sum = 0;
+        if(l1!=NULL && l2!=NULL){
+            sum = (l1->val + l2->val);
             l1 = l1->next;
             l2 = l2->next;
+
         }
-        else if(l1 == NULL){
+        else if(l1==NULL && l2!=NULL){
             sum = l2->val;
             l2 = l2->next;
         }
-        else {
+        else if(l1!=NULL && l2==NULL){
             sum = l1->val;
             l1 = l1->next;
         }
-        result->val = over + sum%10;
-        over = 0;
-        if(sum >= 10){
-            over = 1;
+        result->val = (sum + carry_over)%10;
+
+
+        if(sum+carry_over >= 10){
+            carry_over = 1;
         }
-        //printf("%d",result->val);
+        else{
+            carry_over = 0;
+        }
+        printf("%d\n",result->val);
         struct ListNode* new_node = malloc(sizeof(struct ListNode));
         new_node->next = NULL;
         result->next = new_node;
+        result_ender = result;
         result = result->next;
+        if(l1==NULL && l2== NULL && carry_over==0){
+            quit = 1;
+        }
     }
-    printf("- - - - - - - - - - - - - - - - - -\n");
-    result = NULL;
+    result_ender->next = NULL;
     return result_holder;
 }
